91abefde3e88e1422721e8e0052914b618799c9b
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2023-05-15 19:50:02 +0200

    remove duplicated versions

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,15 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.66"
     }
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
-      version = ">= 1.9.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5"
     }
   }
 }
